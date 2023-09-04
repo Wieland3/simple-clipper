b57bb04fe4b68b3b4283de6801be861bae230541
Wieland Pfesdorf <wielandpfesdorf@gmail.com>
2023-09-04 18:58:29 +0200

    Created PluginProcessor

---
+++ Source/PluginProcessor.cpp
@@ -0,0 +1,227 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+ClipperAudioProcessor::ClipperAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
+                     #endif
+                       ), apvts(*this, nullptr, "PARAMETERS", createParameterLayout())
+#endif
+{
+}
+
+ClipperAudioProcessor::~ClipperAudioProcessor()
+{
+}
+
+//==============================================================================
+juce::AudioProcessorValueTreeState::ParameterLayout ClipperAudioProcessor::createParameterLayout()
+{
+    std::vector<std::unique_ptr<juce::RangedAudioParameter>> parameters;
+
+    // Add parameters to the 'parameters' vector using std::make_unique
+    // For example, to create a gain parameter:
+    parameters.push_back(std::make_unique<juce::AudioParameterFloat>(
+        "gain",         // Parameter ID
+        "Gain",         // Parameter name
+        0.0f,           // Minimum value
+        20.0f,           // Maximum value
+        0.0f));         // Default value
+
+    parameters.push_back(std::make_unique<juce::AudioParameterFloat>("threshold",
+                                                                     "Threshold",
+                                                                     -20.0f,
+                                                                     0.0f,
+                                                                     -1.2f));
+
+    return { parameters.begin(), parameters.end() };
+}
+
+const juce::String ClipperAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool ClipperAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool ClipperAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool ClipperAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double ClipperAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int ClipperAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int ClipperAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void ClipperAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String ClipperAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void ClipperAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+//==============================================================================
+void ClipperAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+}
+
+void ClipperAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool ClipperAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    juce::ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    // Some plugin hosts, such as certain GarageBand versions, will only
+    // load plugins that support stereo bus layouts.
+    if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void ClipperAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    
+    float gain = *apvts.getRawParameterValue("gain");
+    float threshold = *apvts.getRawParameterValue("threshold");
+    
+    float gainAbs = std::pow(10, gain/20);
+    float thresholdAbs = std::pow(10, threshold/20);
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+
+        for (int i = 0; i < buffer.getNumSamples(); i++){
+            channelData[i] = channelData[i] * gainAbs;
+            if (channelData[i] > thresholdAbs){
+                channelData[i] = thresholdAbs;
+            }
+            else if (channelData[i] < (-thresholdAbs)){
+                channelData[i] = -thresholdAbs;
+            }
+        }
+    }
+}
+
+//==============================================================================
+bool ClipperAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+juce::AudioProcessorEditor* ClipperAudioProcessor::createEditor()
+{
+    return new ClipperAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void ClipperAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+    //juce::MemoryOutputStream (destData, true).writeFloat (*gain);
+}
+
+void ClipperAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+    //*gain = juce::MemoryInputStream (data, static_cast<size_t> (sizeInBytes), false).readFloat();
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new ClipperAudioProcessor();
+}
